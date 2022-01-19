d34c11fc099b811767d491b550b25fc7bfa04548
Landon Shay <landonshay@gmail.com>
2022-01-19 16:40:33 -0500

    saved changes this time

--- Game Tracker/DataModels/Game.cs
+++ Game Tracker/DataModels/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,27 @@ namespace Game_Tracker.Models
 {
     public class Game
     {
+        public enum AgeRating
+        {
+            E = 1,
+            E10,
+            T,
+            M
+        }
+        [Key]
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Publisher { get; set; }
+        public AgeRating ESRBRating { get; set; }
+        public double StarRating { get; set; }
 
+        [ForeignKey(nameof(Genre))]
+        public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+
+        [ForeignKey(nameof(GameSystem))]
+        public int GameSystemId { get; set; }
+        public virtual GameSystem GameSystem { get; set; }
     }
 }
