52b4a13d1ce731147dacf12cd4c9492c33009f71
Louis Milrod <int4rweb@gmail.com>
2022-01-20 10:27:32 -0500

    generated a get games alphabetically method

--- Game Tracker/Controllers/GameController.cs
+++ Game Tracker/Controllers/GameController.cs
@@ -1,16 +1,36 @@
 ﻿using Game_Tracker.DataModels;
 using Game_Tracker.DisplayModels;
+using Game_Tracker.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Game_Tracker.Controllers
 {
     public class GameController : ApiController
     {
-        
+        private readonly ApplicationDBContext _context = new ApplicationDBContext();
+
+        [HttpGet]
+        public async Task<IHttpActionResult> GetGamesAlphabetically()
+        {
+            List<Game> games = await _context.Games.ToListAsync();
+            List<GameListItem> gameList = games.Select(g => new GameListItem()
+            {                
+                Name = g.Title
+
+            }).ToList();
+
+            IEnumerable<GameListItem> result = gameList.OrderBy(g => g.Name).ToList();
+
+            return Ok(result);
+
+
+        }
     }
 }
