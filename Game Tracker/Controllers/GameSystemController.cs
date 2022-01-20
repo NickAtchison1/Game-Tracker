b5c0e421d9ebfdb3e9936426f5187844aa2b24a3
Nick Atchison <n.atchison55@gmail.com>
2022-01-20 11:47:24 -0500

    added post method to controllers

--- Game Tracker/Controllers/GameSystemController.cs
+++ Game Tracker/Controllers/GameSystemController.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using Game_Tracker.DataModels;
+using Game_Tracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Game_Tracker.Controllers
 {
     public class GameSystemController : ApiController
     {
+        private readonly ApplicationDBContext _context = new ApplicationDBContext();
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateGameSystem(GameSystem model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            _context.GameSystems.Add(model);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
